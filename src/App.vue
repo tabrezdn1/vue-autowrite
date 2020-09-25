bfeadd5b9fd9dbffa376c88db55f5e449943dab7
tabrezdn1 <tabrezdn1@gmail.com>
2020-09-26 00:39:38 +0530

    Add autowrite code

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,12 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <Autowrite text="Welcome to Your Vue.js App" :delay="100" />
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Autowrite from "./components/AutoWrite";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Autowrite,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/AutoWrite.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="auto-write">
+    <h1>{{ displayText }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AutoWrite",
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    delay: {
+      type: Number,
+      default: 100,
+    },
+  },
+  data() {
+    return {
+      autoWriteText: "",
+      displayText: "",
+      index: 0,
+      delayTime: 0,
+    };
+  },
+  mounted() {
+    // update data property from props
+    this.initData();
+
+    // init writing
+    setInterval(this.initWriting, this.delayTime);
+  },
+  methods: {
+    initData() {
+      this.autoWriteText = this.text;
+      this.delayTime = this.delay;
+    },
+    initWriting() {
+      this.displayText = this.autoWriteText.slice(0, this.index);
+
+      if (this.index > this.autoWriteText.length) {
+				this.index = 0;
+      }
+      this.index++;
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.auto-write {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #17b85a;
+  margin-top: 60px;
+}
+</style>
